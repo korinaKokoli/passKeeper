9fd9b7f97c0c0b2ecef4e262be63c816c1707f4d
korinakok <kokoli.korina@gmail.com>
2019-09-24 16:23:13 +0300

    KATHARISA

--- src/passKeeper/TestForGit.java
+++ src/passKeeper/TestForGit.java
@@ -3,17 +3,10 @@ package passKeeper;
 public class TestForGit {
 
     public void add(){
-        //sxolia sti main
-        //kano allagi na tin do sto remote
-        //allagi me aplo merge 2 me aplo commit oxi push
-        //allagi me rebase 2 me aplo commit oxi push
 
     }
 
     public void remove(){
-        //edo apo git
-        //edo apo git 2
-        //allagi sto branch meta to rebase
 
     }
 
@@ -21,7 +14,3 @@ public class TestForGit {
     int b;
     int v;
 }
-
-//allages apo master - apo master - allgi apo branch
-//allagi 2 meta to rebase tora prepei na vgalei warning
-//den mporei na kanei to merge
